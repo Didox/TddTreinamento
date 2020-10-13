3e83930403312358ff41b61fa83f28187edb259e
Elmon Noronha <elmon.noronha@gmail.com>
2020-10-13 20:53:34 -0300

    Criado factory para clientes e teste do factory

---
+++ Tdd/UClienteFactory.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using TddConsole.Models;
+
+/*
+ Elmon - Criar um fatore para salvar e buscar objetos com o teste 
+ Objeto cliente
+*/ 
+
+namespace Tdd
+{
+    public sealed class UClienteFactory
+    {
+        private static UClienteFactory _instance = null;
+
+        private List<string> _nomes = new List<string>() {"Danilo", "Elmon", "Marco"};
+
+        private List<string> _sobrenomes = new List<string>() {"Santos", "Noronha", "Tomaz"};
+
+        private Random _random;
+
+        UClienteFactory()
+        {
+            _random = new Random();
+        }
+
+        public static UClienteFactory Instance
+        {
+            get { return _instance ?? (_instance = new UClienteFactory()); }
+        }
+
+        public Cliente ClienteMockado()
+        {
+            var tmp = new Cliente();
+            tmp.Id = _random.Next();
+            tmp.Nome = _nomes[_random.Next(0, 2)];
+            tmp.Sobrenome = _sobrenomes[_random.Next(0, 2)];
+            return tmp;
+        }
+
+        public Cliente ClienteMockadoNull()
+        {
+            var tmp = new Cliente();
+            return tmp;
+        }
+    }
+}
